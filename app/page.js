08168c773188d516ed339b3575ab1a76b6def14d
Joann002 <joannrazafinimanana159@gmail.com>
2025-07-01 20:59:07 +0000

    oui

--- app/page.js
+++ app/page.js
@@ -12,8 +12,8 @@ export default function Home() {
         <section className="max-w-7xl mx-auto p-8">
           {/* Titre Principal */}
           <div className="text-center mb-16 space-y-4">
-            <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold tracking-tight bg-gradient-to-r from-primary via-primary/80 to-primary/60 bg-clip-text text-transparent hover:scale-[1.01] transition-transform duration-300 px-2 whitespace-nowrap overflow-x-auto">
-              Joann Michel RAZAFINIMANANA
+            <h1 className="text-[clamp(1.5rem,5vw,4.5rem)] font-bold tracking-tight bg-gradient-to-r from-primary via-primary/80 to-primary/60 bg-clip-text text-transparent hover:scale-[1.01] transition-transform duration-300">
+              Joann Michel<br />RAZAFINIMANANA
             </h1>
             <h2 className="text-2xl md:text-3xl text-muted-foreground">
               Développeur Full Stack
